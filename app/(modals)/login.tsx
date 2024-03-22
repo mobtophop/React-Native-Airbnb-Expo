4a703b0bc3d418107fd1b556e3cc64506e8d43ad
mobtophop <mobtophop@gmail.com>
2024-03-22 21:04:11 +0200

    prepared login functionality

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -2,9 +2,47 @@ import React from 'react';
 import {View, StyleSheet, TextInput, TouchableOpacity, Text} from 'react-native';
 import {useWarmUpBrowser} from "@/hooks/useWarmUpBrowser";
 import {defaultStyles} from "@/constants/Styles";
+import Colors from "@/constants/Colors";
+import {Ionicons} from "@expo/vector-icons";
+import {useOAuth} from "@clerk/clerk-expo";
+import {useRouter} from "expo-router";
+
+enum Strategy {
+    Google = 'oauth_google',
+    Apple = 'oauth_apple',
+    Facebook = 'oauth_facebook',
+}
 
 const Page = () => {
     useWarmUpBrowser();
+
+    const router = useRouter();
+
+    const { startOAuthFlow: googleAuth } = useOAuth({ strategy: 'oauth_google' });
+    const { startOAuthFlow: appleAuth } = useOAuth({ strategy: 'oauth_apple' });
+    const { startOAuthFlow: facebookAuth } = useOAuth({ strategy: 'oauth_facebook' });
+
+    const onSelectAuth = async (strategy: Strategy) => {
+        const selectedAuth = {
+            [Strategy.Google]: googleAuth,
+            [Strategy.Apple]: appleAuth,
+            [Strategy.Facebook]: facebookAuth,
+        }[strategy];
+
+        try {
+            console.log('here')
+            const { createdSessionId, setActive } = await selectedAuth();
+
+            console.log('createdSessionId',createdSessionId);
+            if (createdSessionId) {
+                await setActive!({ session: createdSessionId });
+                router.back();
+                console.log('id', createdSessionId);
+            }
+        } catch (err) {
+            console.error('OAuth error', err);
+        }
+    };
       return (
         <View style={styles.container}>
             <TextInput
@@ -16,6 +54,46 @@ const Page = () => {
             <TouchableOpacity style={defaultStyles.btn}>
                 <Text style={defaultStyles.btnText}>Continue</Text>
             </TouchableOpacity>
+
+            <View style={styles.seperatorView}>
+                <View
+                    style={{
+                        flex: 1,
+                        borderBottomColor: 'black',
+                        borderBottomWidth: StyleSheet.hairlineWidth,
+                    }}
+                />
+                <Text style={styles.seperator}>or</Text>
+                <View
+                    style={{
+                        flex: 1,
+                        borderBottomColor: 'black',
+                        borderBottomWidth: StyleSheet.hairlineWidth,
+                    }}
+                />
+            </View>
+
+            <View style={{ gap: 20 }}>
+                <TouchableOpacity style={styles.btnOutline}>
+                    <Ionicons name="mail-outline" size={24} style={defaultStyles.btnIcon} />
+                    <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Apple)}>
+                    <Ionicons name="logo-apple" size={24} style={defaultStyles.btnIcon} />
+                    <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Google)}>
+                    <Ionicons name="logo-google" size={24} style={defaultStyles.btnIcon} />
+                    <Text style={styles.btnOutlineText}>Continue with Google</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Facebook)}>
+                    <Ionicons name="logo-facebook" size={24} style={defaultStyles.btnIcon} />
+                    <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+                </TouchableOpacity>
+            </View>
         </View>
       );
 };
@@ -26,6 +104,33 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         padding: 26,
+    },
+    seperatorView: {
+        flexDirection: 'row',
+        gap: 10,
+        alignItems: 'center',
+        marginVertical: 30,
+    },
+    seperator: {
+        fontFamily: 'mon-sb',
+        color: Colors.grey,
+        fontSize: 16,
+    },
+    btnOutline: {
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: Colors.grey,
+        height: 50,
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        paddingHorizontal: 10,
+    },
+    btnOutlineText: {
+        color: '#000',
+        fontSize: 16,
+        fontFamily: 'mon-sb',
     },
 });
 export default Page;
