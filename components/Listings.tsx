8b491412b5546ee20e2e7cb7f9b239f2bffe284b
mobtophop <mobtophop@gmail.com>
2024-03-25 18:04:11 +0200

    made parallax effect for details screen image and header, added details screen

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,21 +1,76 @@
-import React, {useEffect} from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {StyleSheet, View, FlatList, ListRenderItem, TouchableOpacity, Image, Text} from 'react-native';
+import {defaultStyles} from "@/constants/Styles";
+import {Link} from "expo-router";
+import {Ionicons} from "@expo/vector-icons";
+import Animated, {FadeInRight, FadeOutLeft} from "react-native-reanimated";
 
 interface Props {
     listings: any[];
     category: string;
 }
-const Listings = ({listings, category}: Props) => {
+const Listings = ({listings: items, category}: Props) => {
+    const [loading, setLoading] = useState(false);
+    const listRef = useRef<FlatList>(null);
+
     useEffect(() => {
-        console.log('listing length,', listings.length )
+        setLoading(true);
+
+        setTimeout(() => {
+            setLoading(false);
+        }, 200)
     }, [category]);
+
+    const renderRow: ListRenderItem<any> = ({item}) => (
+        <Link href={`/listing/${item.id}`} asChild>
+            <TouchableOpacity>
+                <Animated.View style={styles.listing} entering={FadeInRight} exiting={FadeOutLeft}>
+                    <Image source={{uri: item.medium_url}} style={styles.image}/>
+                    <TouchableOpacity style={{position: 'absolute', right: 30, top: 30}}>
+                        <Ionicons name='heart-outline' size={24} color={'#000'} />
+                    </TouchableOpacity>
+
+                    <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                        <Text style={{ fontSize: 16, fontFamily: 'mon-sb'}}>{item.name}</Text>
+
+                        <View style={{flexDirection: 'row', gap: 4}}>
+                            <Ionicons name='star' size={16} />
+                            <Text style={{ fontFamily: 'mon-sb'}}>{item.review_scores_rating / 20}</Text>
+                        </View>
+                    </View>
+
+                    <Text style={{ fontFamily: 'mon'}}>{item.room_type}</Text>
+
+                    <View style={{ flexDirection: 'row', gap: 4 }}>
+                        <Text style={{ fontFamily: 'mon-sb' }}>€ {item.price}</Text>
+                        <Text style={{ fontFamily: 'mon' }}>night</Text>
+                    </View>
+                </Animated.View>
+            </TouchableOpacity>
+        </Link>
+    )
     return (
-        <View>
-            <Text>
-                Listings
-            </Text>
+        <View style={defaultStyles.container}>
+            <FlatList
+                ref={listRef}
+                data={loading ? [] : items}
+                renderItem={renderRow}
+            />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    listing: {
+        padding: 16,
+        gap: 10,
+        marginVertical: 16
+    },
+    image: {
+        width: '100%',
+        height: 300,
+        borderRadius: 10,
+    }
+})
 
 export default Listings;
