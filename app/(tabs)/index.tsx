51d9eeb575b7cc8c3d7bc418182cc0ef84c6de17
mobtophop <mobtophop@gmail.com>
2024-03-26 12:11:41 +0200

    added map with clusters for locations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,10 +5,14 @@ import ExploreHeader from '@/components/ExploreHeader';
 import Listings from "@/components/Listings";
 import listingsData from '@/assets/data/airbnb-listings.json';
 import listingsDataGeo from '@/assets/data/airbnb-listings.geo.json';
+import ListingsMap from "@/components/ListingsMap";
 
 const Page = () => {
-    const items = useMemo(() => listingsData as any, []);
-    const getoItems = useMemo(() => listingsDataGeo, []);
+    const items = useMemo(() => {
+        const filteredListings = listingsData.filter(item => item.medium_url);
+        return filteredListings;
+    }, []);
+    const geToItems = useMemo(() => listingsDataGeo, []);
     const [category, setCategory] = useState<string>('Tiny homes');
 
     const onDataChanged = (category: string) => {
@@ -24,6 +28,7 @@ const Page = () => {
                 }}
             />
             <Listings listings={items} category={category}/>
+            <ListingsMap listings={geToItems} />
         </View>
     );
 };
--- components/Listings.tsx
+++ components/Listings.tsx
@@ -25,26 +25,30 @@ const Listings = ({listings: items, category}: Props) => {
         <Link href={`/listing/${item.id}`} asChild>
             <TouchableOpacity>
                 <Animated.View style={styles.listing} entering={FadeInRight} exiting={FadeOutLeft}>
-                    <Image source={{uri: item.medium_url}} style={styles.image}/>
-                    <TouchableOpacity style={{position: 'absolute', right: 30, top: 30}}>
-                        <Ionicons name='heart-outline' size={24} color={'#000'} />
-                    </TouchableOpacity>
+                    {item.medium_url ? (
+                        <>
+                            <Image source={{uri: item.medium_url}} style={styles.image}/>
+                            <TouchableOpacity style={{position: 'absolute', right: 30, top: 30}}>
+                                <Ionicons name='heart-outline' size={24} color={'#000'} />
+                            </TouchableOpacity>
 
-                    <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                        <Text style={{ fontSize: 16, fontFamily: 'mon-sb'}}>{item.name}</Text>
+                            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                                <Text style={{ fontSize: 16, fontFamily: 'mon-sb'}}>{item.name}</Text>
 
-                        <View style={{flexDirection: 'row', gap: 4}}>
-                            <Ionicons name='star' size={16} />
-                            <Text style={{ fontFamily: 'mon-sb'}}>{item.review_scores_rating / 20}</Text>
-                        </View>
-                    </View>
+                                <View style={{flexDirection: 'row', gap: 4}}>
+                                    <Ionicons name='star' size={16} />
+                                    <Text style={{ fontFamily: 'mon-sb'}}>{item.review_scores_rating / 20}</Text>
+                                </View>
+                            </View>
 
-                    <Text style={{ fontFamily: 'mon'}}>{item.room_type}</Text>
+                            <Text style={{ fontFamily: 'mon'}}>{item.room_type}</Text>
 
-                    <View style={{ flexDirection: 'row', gap: 4 }}>
-                        <Text style={{ fontFamily: 'mon-sb' }}>€ {item.price}</Text>
-                        <Text style={{ fontFamily: 'mon' }}>night</Text>
-                    </View>
+                            <View style={{ flexDirection: 'row', gap: 4 }}>
+                                <Text style={{ fontFamily: 'mon-sb' }}>€ {item.price}</Text>
+                                <Text style={{ fontFamily: 'mon' }}>night</Text>
+                            </View>
+                        </>
+                    ): null}
                 </Animated.View>
             </TouchableOpacity>
         </Link>
